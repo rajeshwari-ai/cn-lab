670c5aa30e3c3b09f9be33d1a0dfaff02e48d55a
rajeshwari-ai <mandarajeshwari16@gmail.com>
2024-11-12 15:30:27 +0530

    Add files via upload

---
+++ DATAENCR.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,x;
+char str[100];
+printf("\nPlease enter a string:\t");
+gets(str);
+printf("\nPlease choose following options:\n");
+printf("1 = Encrypt the string.\n");
+printf("2 = Decrypt the string.\n");
+scanf("%d",&x);
+switch(x)
+{
+case 1:
+for(i=0;(i<100&&str[i] !='\0');i++)
+str[i]=str[i]+3;
+printf("\n encrypted string: %s\n",str);
+break;
+case2:
+for(i=0;(i<100&&str[i]!='\0');i++)
+str[i]=str[i]-3;
+printf("\nDecrypted string:%s\n",str);
+break;
+default:
+printf("\nError\n");
+}
+return 0;
+}
